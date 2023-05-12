479d130d6962b1ce273b6d6e4d2ccaface7b5bf9
Emil <emil88pl@gmail.com>
2023-05-12 15:04:14 +0100

    queue :

--- queue1.py
+++ queue1.py
@@ -3,14 +3,12 @@ import os
 
 def my_func(*args):
     queue = args[0]
-    # words.txt
 
     word = ""
     while word != "END":
         word = queue.get()
         if len(word) == 15:
-            print(os.getpid(), ": ", word)
-
+            print(os.getpid(), ":", word)
 
 def main():
     queue = Queue()
@@ -18,26 +16,20 @@ def main():
     p1 = Process(target=my_func, args=(queue, "1"))
     p2 = Process(target=my_func, args=(queue, "2"))
 
-
     p1.start()
     p2.start()
 
-
-
-    for line in open("words.txt"):
-        queue.put(line[-1]) 
-
+    for line in open('words.txt'):
+            queue.put(line[:-1]) 
 
     queue.put("END")
     queue.put("END")
-
 
     p1.join()
     p2.join()
-
-
     print("ALl done!")
 
 
 if __name__ == "__main__": # have to be in main
-    main()
+     main()
+
