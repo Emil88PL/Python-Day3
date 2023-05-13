90638ac92b56be8207538d5548f0e8188f07c20f
Emil <emil88pl@gmail.com>
2023-05-13 14:37:34 +0100

    Pythonic 2 _

---
+++ testss.py
@@ -0,0 +1,61 @@
+# def main():
+#     times = int(input("How many times? "))
+#     cat_do(times)
+#     dog_do(times)
+
+
+# def cat_do(times):
+#     x = "meow"
+#     i = 1 # from 1 for reading - Better to start from 0 
+#     while i < times:
+#         print(f"{x} : {i} times")
+#         i += 1
+
+# def dog_do(times):
+#     x = "bark"
+#     i = 1
+#     while i < times:
+#         print(f"{x} : {i} times")
+#         i += 1
+
+# main()
+
+# # square = assignment
+# # ttriangle = question
+# # circle = start / stop
+
+# list
+
+for _ in range(11): # Pythonic version
+    print("Moew")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
