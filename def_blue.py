f825a71f2c789eaf3185cee221e3b7e3bcf9d092
Emil <emil88pl@gmail.com>
2023-05-13 12:05:53 +0100

    def_main_try

---
+++ def_blue.py
@@ -0,0 +1,27 @@
+def main():
+    name = input("What's your name? ")
+    hello(name)
+    
+
+def hello(to="World!"):
+    print("Hello,", to)
+
+
+
+def calc():
+    try:
+        while True:
+            x = int(input("Enter a number "))
+            print(f"Square of {x} is:", square(x))
+    except:
+        error()
+
+def square(x):
+    return x * x
+
+def error():
+    print("Only Numbers please!")
+    calc()
+
+main()
+calc()
