dd9c337be8e8d36f643264d94323d05059c17471
Emil <emil88pl@gmail.com>
2023-05-14 19:50:12 +0100

    matches :=

--- regEx/twitt.py
+++ regEx/twitt.py
@@ -3,7 +3,7 @@ url = input("URL: ").strip()
 
 # non-capturing version
 
-if matches := re.search(r"^(https?://)?(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
+if matches := re.search(r"^(https?://)?(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE):
  print(f"Username: ", matches.group(2))
     
 
