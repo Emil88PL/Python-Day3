841e6944c3b7b1081b7e03e326a1b2cd2bbe8a82
Emil <emil88pl@gmail.com>
2023-05-14 13:34:26 +0100

    use of lambda

--- File I.O/student5.py
+++ File I.O/student5.py
@@ -8,11 +8,6 @@ with open("students.csv") as file:
        # student['house'] = house
         students.append(student)
 
-def get_name(student):
-    return student['name']
 
-def get_house(student):
-    return student['house']
-
-for student in sorted(students, key=get_house, reverse=True):
+for student in sorted(students, key=lambda student : student["name"], reverse=True):
     print(f"{student['name']} is in {student['house']}")
