a1b4c7ed4a9a8e6686d5dd7927dd30e645fd960c
Emil <emil88pl@gmail.com>
2023-05-14 15:06:08 +0100

    reader(file, skipinitialspace=True)

--- File I.O/student5.py
+++ File I.O/student5.py
@@ -4,10 +4,8 @@ with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",") 
         student = {"name": name, "house": house}
-       # student['name'] = name
-       # student['house'] = house
         students.append(student)
 
 
-for student in sorted(students, key=lambda student : student["name"], reverse=True):
+for student in sorted(students, key=lambda student : student["name"], reverse=True): 
     print(f"{student['name']} is in {student['house']}")
---
+++ File I.O/students_2.2.py
@@ -0,0 +1,31 @@
+# it works with skipinitialspace=True !!!
+
+import csv
+
+students = []
+
+with open("File I.O/students2.csv") as file:
+    reader = csv.reader(file, skipinitialspace=True)   
+    for name, home in reader:
+        students.append({"name": name, "home": home})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is from {student['home']}")
+
+
+
+## belove example dosent work - https://cs50.harvard.edu/python/2022/notes/6/ copy from harvard webstie still not working as intended
+## reading only half line
+
+
+# import csv
+
+# students = []
+
+# with open("File I.O/students2.csv") as file:
+#     reader = csv.reader(file)
+#     for row in reader:
+#         students.append({"name": row[0], "home": row[1]})
+
+# for student in sorted(students, key=lambda student: student["name"]):
+#     print(f"{student['name']} is from {student['home']}")
