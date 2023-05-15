446589be5041ef1a5af4089baec01a86756455bb
Emil <emil88pl@gmail.com>
2023-05-15 12:02:51 +0100

    class type

---
+++ type.py
@@ -0,0 +1,23 @@
+print(type(50))
+print(type("ELo"))
+print(type(50.0))
+print(type(True))
+print(type([]))
+print(type({}))
+print(type(()))
+print(type((((((())))))))
+#classes
+
+# instance variables
+# class methods
+
+# @classmethod
+
+
+
+
+
+
+
+
+
